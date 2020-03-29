def830f1164845a05c53e736dc10d600804607f5
Tim Dime <tim@timdime.com>
2020-03-28 21:11:41 -0500

    Add files via upload

---
+++ TD/config/credentials.py
@@ -0,0 +1,8 @@
+
+client_id = #######
+account_id = #######
+password = #######
+password2 = #######
+account_number = #######
+redirect_uri = 'http://localhost:8080'
+email_address = #######
---
+++ TD/config/model_params.py
@@ -0,0 +1,34 @@
+direction = 2.5
+bucket_weights = [.1, .3, .7, .9, 1]
+num_of_buckets = 5.0
+minHighLessLow = .20
+mincvHighLessLow_bucket = .30
+minTrades = 5
+minVolume = 50
+candlestick_span_in_sec = 59
+max_notional_value = 5000.0
+max_quantity = 1
+max_share_price = 750.0
+max_global_trades = 25
+start_time = 835
+end_time = 1415
+mode = 'Testing'
+# mode = 'Production'
+
+
+#Night Testing
+# direction = -.05
+# num_of_buckets = 3.0
+# bucket_weights = [.1, .3, .7, .9, 1]
+# minHighLessLow = .01
+# mincvHighLessLow_bucket = 10000.5
+# minTrades = 1
+# minVolume = 1
+# candlestick_span_in_sec = 15
+# max_notional_value = 11000
+# max_quantity = 250
+# max_share_price = 10000
+# max_global_trades = 5
+# start_time = 1
+# end_time = 2400
+# mode = 'Testing'
